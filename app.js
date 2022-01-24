bc2290c0c09948b0dff2b92621df6e68b214b4b8
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-01-24 15:28:37 +0000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const testController = require('./controllers/testController');
+const testController = require('./controllers/Controller.js');
 
 
 var port = process.env.PORT || 8080;
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 app.get('/', testController.getTestData);
 app.get('/otherData', testController.getSecondTestData);
+app.get('/disruptions', testController.getAllBusDisruptions);
 
 app.listen(port, () => {
 	console.log(`app is listening to port ${port}`);
---
+++ controllers/Controller.js
@@ -0,0 +1,43 @@
+const Test = require('../models/Test');
+const fetch = require("node-fetch");
+const Disruption = require("../models/Journey");
+
+exports.getTestData = (req, res) => {
+	let data = [
+		new Test('Test name', 12),
+		new Test('Second name', 13)
+	];
+	res.render('testView', {
+		data : data,
+	});
+};
+
+exports.getSecondTestData = (req, res) => {
+	let data = [
+		new Test('other name', 15),
+		new Test('other second name', 16)
+	];
+	res.render('testView', {
+		data : data,
+	});
+};
+
+exports.getAllBusDisruptions = async (req, res) => {
+	const url = "https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption";
+
+	const response = await fetch(url);
+	const disruptionsResponse = await response.json();
+
+	const disruptions = disruptionsResponse.map(
+		dis => new Disruption(
+			dis.commonName,
+			dis.description,
+			dis.fromDate,
+			dis.toDate
+		)
+	);
+
+	res.render('JourneyList', {
+		data: disruptions,
+	});
+};
